02f3cf9dec0276d8bc106f915d2e9376bb4e89bf
anabravn <bravinheloisa@gmail.com>
2023-07-13 14:58:29 -0300

    Documentação da função mm_p

--- mm.c
+++ mm.c
@@ -4,29 +4,54 @@
 
 #include "mm.h"
 
+/**
+ * @brief Multiplicação de matrizes em paralelo
+ *
+ * Realiza a multiplicação de matrizes em paralelo, dividindo a matriz
+ * em blocos com aproximadamente a mesma quantidade de linhas para cada fluxo.
+ * 
+ * @param a Matriz A de ordem m x n
+ * @param b Matriz B de ordem n x p
+ * @param c Matriz C resultante, de ordem m x p
+ *
+ * @param m Número de linhas da matriz A e número de linhas da matriz resultante
+ * @param n Número de linhas da matriz B e número de colunas da matriz resultante
+ * @param p Número de colunas da matriz B e número  
+ *
+ * @param f Número de fluxos de execução
+*/
 void mm_p(double **a, double **b, double **c, int m, int n, int p, int f) {
     pthread_t *id = malloc(sizeof(pthread_t) * f);
     mm_args *args = (mm_args *) malloc(sizeof(mm_args) * f);
-    int q, r;
+    int quociente, resto;
 
-    q = m / f;
-    r = m % f;
+    /* Define a quantidade de linhas para cada fluxo */;
+    quociente = m / f;
+    resto = m % f;
 
+    /**
+     * Preenche um vetor com os argumentos para cada fluxo, definidos
+     * pela estrutura mm_args
+     */
     for (int i = 0; i < f; i++) {
+        /* Endereço das matrizes */
         args[i].a = a;
         args[i].b = b;
         args[i].c = c;
 
+        /* Tamanho das matrizes */
         args[i].m = m;
         args[i].n = n;
         args[i].p = p;
 
-        args[i].i_start = i * q;
-        args[i].i_end = args[i].i_start + q;
+        /* Linha inicial e linha final para cada fluxo */
+        args[i].i_start = i * quociente;
+        args[i].i_end = args[i].i_start + quociente;
 
     }
 
-    args[f - 1].i_end += r; // resto
+    /* Atribui as linhas restantes para o último fluxo */
+    args[f - 1].i_end += resto;
 
     for (int i = 0; i < f; i++) {
         pthread_create(&(id[i]), NULL, mm_aux, (void *) &(args[i]));
